b79af82a0f0f78148b422440a3b01ac2e477186b
Olivia Dahlen <101267945+MissPotatoGirl@users.noreply.github.com>
2022-09-14 17:56:29 +0200

    Current location added

--- src/index.js
+++ src/index.js
@@ -6,12 +6,28 @@ function displayPlaceholder(){
     axios.get(apiUrl).then(getInfo);
 }
 
+function displayTimeStamp(){
+    let now = new Date();
+
+    let getDay = now.getDay();
+    let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = dayName[getDay];
+
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let time = `${hour}.${minutes}`;
+
+    let timeStap = `${day} ${time}`;
+    let timeStapElement = document.querySelector(".timestamp");
+    timeStapElement.innerHTML = timeStap;
+}
+
 function searchCity(event) {
     event.preventDefault();
     getCityName();
   }
   
-  function getCityName(position) {
+function getCityName(position) {
     let searchInput = document.querySelector("#search-input");
     let searchCity = document.querySelector(".cityName");
     searchCity.innerHTML = searchInput.value;
@@ -21,10 +37,25 @@ function searchCity(event) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&appid=${apiKey}`;
   
     axios.get(apiUrl).then(getInfo);
+}
+
+function displayCurrentLocation(event) {
+    event.preventDefault();
+  
+    navigator.geolocation.getCurrentPosition(getCurrentLocation);
+  }
+  
+function getCurrentLocation(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiKey = "7fa08a5cce73b5c1c5b2cb4406f7781a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+  
+    axios.get(apiUrl).then(getInfo);
   }
 
 function getInfo(response){
-    console.log(response.data);
+    //console.log(response.data);
 
     let cityName = response.data.name;
     let cityNameElement = document.querySelector(".cityName");
@@ -61,24 +92,13 @@ function getInfo(response){
 }
 
 
-function displayTimeStamp(){
-    let now = new Date();
 
-    let getDay = now.getDay();
-    let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let day = dayName[getDay];
-
-    let hour = now.getHours();
-    let minutes = now.getMinutes();
-    let time = `${hour}.${minutes}`;
-
-    let timeStap = `${day} ${time}`;
-    let timeStapElement = document.querySelector(".timestamp");
-    timeStapElement.innerHTML = timeStap;
-}
 
 displayTimeStamp();
 displayPlaceholder();
 
 let searchButton = document.querySelector("#search-city");
 searchButton.addEventListener("click", searchCity);
+
+let currentLocationButton = document.querySelector("#current-position");
+currentLocationButton.addEventListener("click", displayCurrentLocation);
